8fd4b118a0fdc3e1e3f397a1d432f8a76a4afed4
Promise Fru <promisefru1@gmail.com>
2021-01-09 10:28:27 +0100

    initialized variables

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,21 @@
-def add_time(start, duration):
+def add_time(start, duration, day = ""):
+    splitStart = start.split(':')
+    splitStart2 = splitStart[1].split();
+    splitDuration = duration.split(':')
+
+    # start variables
+    startHour = int(splitStart[0])
+    startMin = int(splitStart2[0])
+    startSign = splitStart2[1]
+
+    # duration variables
+    durationHour = int(splitDuration[0])
+    durationMin = int(splitDuration[1])
+
+    print(splitStart, splitStart2,splitDuration)
+    print(startHour, startMin, startSign)
 
 
+add_time("3:30 PM", "2:12")
 
-
-
-    return new_time
+    # return new_time
